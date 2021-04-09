18ae481f37622678cea592186e9298609652405c
CameronFarquhar <cameronfarquhar@gmail.com>
2021-04-09 18:43:18 -0400

    built a function to gather demographic data

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,46 @@
 // Fetch the JSON data
-d3.json("samples.json").then(function(data) {
+
+// @param {array} rows
+// @param {integer} index
+
+// function unpack(rows, index) {
+//   return rows.map(function(row) {
+//     return row[index];
+//   });
+// }
+
+
+function demographics(id){
+  d3.json("static/js/samples.json").then(function(data) {
+    var metadata =  data.metadata;
+    console.log(metadata);
+    var panel = d3.select("#sample-metadata");
+    var filteredInput = metadata.filter(results => results.id.toString() === id)[0];
+    panel.html("");
+    
+    Object.defineProperties(filteredInput).forEach((key)=> {
+      panel.append("ul").text(key[0] + ": " + key[1]);
+    });
+  });
+}
+
+demographics();
+
+
+function buildPlot() {
+  d3.json("static/js/samples.json").then(function(data) {
+  // console.log(data);
+  // var metadataID = data.metadata[0].id;
+  // console.log(metadataID);
+
+
 
   var sampleIndZero = data.samples[0];
 
   var sampleValues = sampleIndZero.sample_values;
 
   var sampleIDs = sampleIndZero.otu_ids;
+  
   console.log(sampleIndZero)
   
   var sampleLabels = sampleIndZero.otu_labels;
@@ -54,8 +89,6 @@ d3.json("samples.json").then(function(data) {
   Plotly.newPlot("bar", chartData, layout);
 
 
-
-
 // Bubble plot
 
 var trace1 = {
@@ -80,59 +113,7 @@ var layout = {
 
 Plotly.newPlot('bubble', data, layout);
 
-var idNum = sampleIndZero.id;
-console.log(idNum);
 
-// d3.append("ul").text(idNum);
+});}
 
-// var wfreqNum = data.metadata[0];
-
-// console.log(wfreqNum);
-
-// part of data to input
-// var traceGauge = {
-//   type: 'pie',
-//   showlegend: false,
-//   hole: 0.4,
-//   rotation: 90,
-//   values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
-//   text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
-//   direction: 'clockwise',
-//   textinfo: 'text',
-//   textposition: 'inside',
-//   marker: {
-//     colors: ['','','','','','','','','','white'],
-//     labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
-//     hoverinfo: 'label'
-//   }
-// }
-
-// // needle
-// var degrees = 50, radius = .9
-// var radians = degrees * Math.PI / 180
-// var x = -1 * radius * Math.cos(radians) * wfreqNum
-// var y = radius * Math.sin(radians)
-
-// var gaugeLayout = {
-//   shapes: [{
-//     type: 'line',
-//     x0: 0.5,
-//     y0: 0.5,
-//     x1: 0.6,
-//     y1: 0.6,
-//     line: {
-//       color: 'black',
-//       width: 3
-//     }
-//   }],
-//   title: 'Chart',
-//   xaxis: {visible: false, range: [-1, 1]},
-//   yaxis: {visible: false, range: [-1, 1]}
-// }
-
-// var dataGauge = [traceGauge]
-
-// Plotly.plot('gauge', dataGauge, gaugeLayout)
-
-
-});
+buildPlot();
